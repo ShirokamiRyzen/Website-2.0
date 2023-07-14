c49bf98ffc297dc724af8f5279b7a4edf6e74a90
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-07-14 11:38:20 +0700

    modified:   ytmusic_conf.js

--- ytmusic_conf.js
+++ ytmusic_conf.js
@@ -2,7 +2,7 @@
 //---   WEB CONFIG  ---// 
 //=====================//
 
-//  revanced.html   //
+//  ytmusic.html   //
 //  Revanced Direct Link
 const spanMusic_1 = document.getElementById("music_direct");
 let direct = "<a type='button' target='_blank' href='https://cloud.ryzendesu.com/api/raw/?path=/Music_ReVanced/youtube-music-revanced_v5.51.50.apk' class='bg-blue-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center tombolkanan'> <svg class='fill-current w-4 h-4 mr-2' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'> <path d='M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z' /> </svg>Direct Link</a>";
